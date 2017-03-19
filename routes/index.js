8ec37cd83cd9b8dffbf9182ae9d9510ef1ffec1b
Arpit Goyal <arpit.go4@gmail.com>
2017-03-19 14:03:33 +0530

    regitration api implmented

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,8 @@ router.post('/login', (req, res, next) => {
 	const { username, password } = req.body;
 	User.find({ username, password }, (err, user) => {
 		if(!user){
-			res.send({ msg: 'Username/Password may be incorrect!'});
+			res.send({ msg: 'Username/Password may be incorrect!', token: null, user: null });
 		}
-		console.log('user', config.JWT_SECRET_KEY);
 		const payload = { id: user.id };
 		const token = jwt.sign(payload, config.JWT_SECRET_KEY);
 		res.send({ msg: 'Login Successfull', token, user: user[0] });
@@ -21,7 +20,19 @@ router.post('/login', (req, res, next) => {
 });
 
 router.post('/register', (req, res, next) => {
-
+	const { firstname, lastname, username, password } = req.body;
+	User.find({ username }, (err, user) => {
+		if(user.length > 0){
+			res.send({ msg: 'User with this username already exists!!' });
+		}else {
+			new User({ firstname, lastname, username, password }).save((err) => {
+				if(err){
+					res.send({ msg: 'Error occured during registration!!' });
+				}
+				res.send({ msg: 'Registration successfull!', user: { firstname, lastname, username, password } });
+			});
+		}
+	});
 });
 
 router.get('/hello', (req, res, next) => res.send({ msg: 'Hello, ExpressJS! '}));
