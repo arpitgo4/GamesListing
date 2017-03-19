d56e8ccfde48a7690efccf481af01239939ded68
Arpit Goyal <arpit.go4@gmail.com>
2017-03-19 11:55:39 +0530

    csv file reader and mongodb data importer implmeneted

--- config/LoadMongo.js
+++ config/LoadMongo.js
@@ -1,11 +1,48 @@
 const fs = require('fs');
 const Game = require('../models').GAME;
 
-fs.readFile(`${__dirname}/games_listing.csv`, (err, data) => {
-	if(err) {
-		console.log(err);
-		return;
-	}
+const readData = (callback) => {
+	fs.readFile(`${__dirname}/games_listing.csv`, (err, data) => {
+		if(err) {
+			console.log(err);
+			return;
+		}
 
-	console.log(data.toString());
+		callback(data.toString());
+	});
+}
+
+const convertToModels = (data) => {
+	// spliting and slicing the header.
+	const splittedLines = data.split('\n');
+	const lines = splittedLines.slice(1, splittedLines.length);
+
+	const games = lines.map(line => {
+		const [ title, platform, score, genre, editors_choice ] = line.split(',');
+		return new Game({ title, platform, score, genre, editors_choice });	
+	});
+	
+	return games;
+}
+
+const removeAllGames = () => {
+	Game.remove({}, (err) => {
+		if(err){
+			console.log(err);
+			return;
+		}
+		console.log('All Records removed!!');
+	});
+}
+
+const insertGames = (games) => {
+	games.forEach(game => game.save());
+}
+
+readData((fileData) => {
+	const games = convertToModels(fileData);
+	removeAllGames();
+	insertGames(games);
 });
+
+
