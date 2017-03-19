fda7e6bae323dfe5c03a769f5c69cb8ffafa9507
Arpit Goyal <arpit.go4@gmail.com>
2017-03-19 16:13:42 +0530

    footer designed

--- client/src/components/Footer/Footer.component.js
+++ client/src/components/Footer/Footer.component.js
@@ -3,13 +3,12 @@ import React from 'react';
 export default class Footer extends React.Component {
 	render(){
 		return (
-			<div className="container">
-				<div className="row">
+			<div className="navbar navbar-default navbar-fixed-bottom">
+				<div id="footer-row" className="row">
 					<div className="col-sm-5 col-xs-5 col-md-5 col-lg-5 col-sm-offset-1">
-						&copy; Capillary Technologies, 2015 All Rights Reserved. Privacy Policy
+						<p id="footer-row-p">&copy; Capillary Technologies, 2017 All Rights Reserved. Privacy Policy</p>
 					</div>
 					<div className="col-sm-4 col-xs-4 col-md-4 col-lg-4 col-sm-offset-7">
-						<i class="ionicons ion-social-facebook-outline"></i>
 					</div>
 				</div>
 			</div>
